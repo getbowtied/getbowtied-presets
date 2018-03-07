684e60bd2f7c197c74467ac130e8a9ba815c48ae
Traian Stanciu <stanciu.traian@gmail.com>
2018-03-07 10:23:37 +0200

    init

---
+++ getbowtied-presets.php
@@ -0,0 +1,42 @@
+<?php
+	/**
+	 * Plugin Name: GetBowtied Presets
+	 * Plugin URI: https://getbowtied.com/
+	 * Description: A suite of tools to help you kickstart your GetBowtied theme.
+	 * Version: 1.0
+	 * Author: GetBowtied
+	 * Author URI: https://getbowtied.com
+	 * Requires at least: 4.9
+	 * Tested up to: 4.9
+	 *
+	 * @package  GetBowtied Presets
+	 * @author GetBowtied
+	 */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+add_action('admin_menu', 'getbowtied_presets_menu');
+ 
+function getbowtied_presets_menu(){
+    add_menu_page( 'GetBowtied Presets', 'GetBowtied Presets', 'manage_options', 'getbowtied-presets', 'getbowtied_presets_main' );
+}
+ 
+function getbowtied_presets_main(){
+	if (isset($_GET['changeset_id']) && is_numeric($_GET['changeset_id']))
+		include dirname( __FILE__ ) . "/includes/templates/single.php";
+	else
+		include dirname( __FILE__ ) . "/includes/templates/main.php";
+}
+
+function load_styles() {
+	wp_enqueue_style( 'getbowtied-presets-css', plugins_url( 'assets/css/styles.css', __FILE__ ), array(), '1.0' );
+}
+
+function load_scripts() {
+	wp_enqueue_script( 'getbowtied-presets-js', plugins_url( 'assets/js/scripts-dist.js', __FILE__ ), array(), '1.0' );
+}
+
+add_action( 'admin_enqueue_scripts', 'load_styles' );
+add_action( 'admin_enqueue_scripts', 'load_scripts' );
