a112beff724905c7b5a5d2dd030444bd17848836
Traian Stanciu <stanciu.traian@gmail.com>
2019-06-25 16:00:27 +0300

    v.1.1

--- getbowtied-presets.php
+++ getbowtied-presets.php
@@ -17,39 +17,53 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-add_action('admin_menu', 'getbowtied_presets_menu');
- 
-function getbowtied_presets_menu(){
-    add_menu_page( 'GetBowtied Presets', 'GetBowtied Presets', 'manage_options', 'getbowtied-presets', 'getbowtied_presets_main' );
-}
- 
-function getbowtied_presets_main(){
-	if (isset($_GET['changeset_id']) && is_numeric($_GET['changeset_id']))
-		include dirname( __FILE__ ) . "/includes/templates/single.php";
-	else
-		include dirname( __FILE__ ) . "/includes/templates/main.php";
-}
+// Plugin Updater
+require 'core/updater/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/getbowtied/getbowtied-presets/master/core/updater/assets/plugin.json',
+	__FILE__,
+	'getbowtied-presets'
+);
 
-function load_styles() {
-	wp_enqueue_style( 'getbowtied-presets-css', plugins_url( 'assets/css/styles.css', __FILE__ ), array(), '1.0' );
-}
+class GetBowtied_Presets {
+	
+	function __construct() {
+		add_action('admin_menu', array($this, 'getbowtied_presets_menu'));
+		add_action( 'admin_enqueue_scripts', array($this, 'load_styles' ));
+		add_action( 'admin_enqueue_scripts', array($this, 'load_scripts' ));
+		add_action( 'after_setup_theme', array($this, 'getbowtied_presets_redirect_url'));
+	}
+	
+	 
+	function getbowtied_presets_menu(){
+	    add_menu_page( 'GetBowtied Presets', 'GetBowtied Presets', 'manage_options', 'getbowtied-presets', array($this,'getbowtied_presets_main'));
+	}
+	 
+	function getbowtied_presets_main(){
+		if (isset($_GET['changeset_id']) && is_numeric($_GET['changeset_id']))
+			include dirname( __FILE__ ) . "/includes/templates/single.php";
+		else
+			include dirname( __FILE__ ) . "/includes/templates/main.php";
+	}
 
-function load_scripts() {
-	wp_enqueue_script( 'getbowtied-presets-js', plugins_url( 'assets/js/scripts-dist.js', __FILE__ ), array(), '1.0' );
-}
+	function load_styles() {
+		wp_enqueue_style( 'getbowtied-presets-css', plugins_url( 'assets/css/styles.css', __FILE__ ), array(), '1.0' );
+	}
 
-add_action( 'admin_enqueue_scripts', 'load_styles' );
-add_action( 'admin_enqueue_scripts', 'load_scripts' );
+	function load_scripts() {
+		wp_enqueue_script( 'getbowtied-presets-js', plugins_url( 'assets/js/scripts-dist.js', __FILE__ ), array(), '1.0' );
+	}
 
-function getbowtied_presets_redirect_url() {
-	if (isset($_REQUEST['customize_changeset_uuid_gbt'])) {
-		$current_url="//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-		$parsed = explode('?', $current_url);
-		$parsed[1]= 'customize_changeset_uuid=' . $_GET['customize_changeset_uuid_gbt'];
-		$parsed = implode($parsed, '?');
-		wp_redirect( $parsed );
-		exit();
+	function getbowtied_presets_redirect_url() {
+		if (isset($_REQUEST['customize_changeset_uuid_gbt'])) {
+			$current_url="//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+			$parsed = explode('?', $current_url);
+			$parsed[1]= 'customize_changeset_uuid=' . $_GET['customize_changeset_uuid_gbt'];
+			$parsed = implode($parsed, '?');
+			wp_redirect( $parsed );
+			exit();
+		}
 	}
 }
 
-add_action( 'after_setup_theme', 'getbowtied_presets_redirect_url');
+new GetBowtied_Presets;
