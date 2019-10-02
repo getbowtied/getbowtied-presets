7b54553b336351f90382923fd5e265768fa8f70d
Vanesa Reinerth <vanesa@getbowtied.com>
2019-10-02 12:24:54 +0300

    delete unuseful

--- getbowtied-presets.php
+++ getbowtied-presets.php
@@ -84,9 +84,7 @@ class Getbowtied_Presets {
 		$this->plugin_name = 'getbowtied-presets';
 
 		$this->load_dependencies();
-		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -96,9 +94,7 @@ class Getbowtied_Presets {
 	 * Include the following files that make up the plugin:
 	 *
 	 * - Getbowtied_Presets_Loader. Orchestrates the hooks of the plugin.
-	 * - Getbowtied_Presets_i18n. Defines internationalization functionality.
 	 * - Getbowtied_Presets_Admin. Defines all hooks for the admin area.
-	 * - Getbowtied_Presets_Public. Defines all hooks for the public side of the site.
 	 *
 	 * Create an instance of the loader which will be used to register the hooks
 	 * with WordPress.
@@ -115,40 +111,11 @@ class Getbowtied_Presets {
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-getbowtied-presets-loader.php';
 
 		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		require_once plugin_dir_path( __FILE__ ) . 'includes/class-getbowtied-presets-i18n.php';
-
-		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once plugin_dir_path( __FILE__ ) . 'admin/class-getbowtied-presets-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( __FILE__ ) . 'public/class-getbowtied-presets-public.php';
-
 		$this->loader = new Getbowtied_Presets_Loader();
-
-	}
-
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Getbowtied_Presets_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function set_locale() {
-
-		$plugin_i18n = new Getbowtied_Presets_i18n();
-
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
 	}
 
@@ -165,22 +132,6 @@ class Getbowtied_Presets {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Getbowtied_Presets_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
